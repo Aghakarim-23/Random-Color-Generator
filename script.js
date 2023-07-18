965e4e27621a475713f2ae9eb0ff341139861b2b
Aghakarim-dev <hemidzade1010@gmail.com>
2023-07-18 12:30:50 +0400

    i edited some codes

---
+++ script.js
@@ -0,0 +1,12 @@
+function getNewColor() {
+  var symbols, color;
+  symbols = "01234567890ABCDEF";
+  color = "#";
+
+  for (var i = 0; i < 6; i++) {
+    color = color + symbols[Math.floor(Math.random() * 16)];
+  }
+
+  document.body.style.background = color;
+  document.getElementById("hex").innerHTML = color;
+}
